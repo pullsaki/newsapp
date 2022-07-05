a54902a815bfbb4d6040e3477f1c8a9ecee4e8ee
pullsaki <saketh.pulluri2002@gmail.com>
2022-07-05 13:02:41 +0530

    Setup and Complete First Task

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Counter App'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visibility = true;
+
+  void _toggleVisibility() {
+    setState(() {
+      _visibility = !_visibility;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Visibility(
+              visible: true,
+              child: Text(
+                'Click the button to hide the below text.',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Container(
+              height: 50,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Visibility(
+                visible: _visibility,
+                child: Text(
+                  'Make me disappear!',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(height: 25.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleVisibility,
+        tooltip: 'Visible',
+        child: Icon(_visibility ? Icons.visibility_off : Icons.visibility),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
